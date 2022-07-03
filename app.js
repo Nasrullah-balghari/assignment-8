62f46651c211fd253632271e734e42b8391440eb
Nasrullah-balghari <nasrullahbalghari676@gmail.com>
2022-07-03 16:01:12 +0500

    assignment-8

---
+++ app.js
@@ -0,0 +1,67 @@
+//Question-1
+// var userInput = prompt("Enter your number in positive")
+// var positive = (Number(userInput));
+// document.write("number: "+positive);
+// document.write("<br>")
+// document.write("round off value: "+Math.round(positive));
+// document.write("<br>")
+// document.write("floor value: "+Math.floor(positive));
+// document.write("<br>")
+// document.write("ceil value: "+Math.ceil(positive));
+
+//Question-2
+// var userInput = prompt("Enter your number in \nnegative floating point")
+// var negative = (Number(userInput));
+// document.write("number: "+negative);
+// document.write("<br>")
+// document.write("round off value: "+Math.round(negative));
+// document.write("<br>")
+// document.write("floor value: "+Math.floor(negative));
+// document.write("<br>")
+// document.write("ceil value: "+Math.ceil(negative));
+
+//Question-3
+// var input = +prompt("Enter the number\nconvert in absolute value")
+// var absoluteValue = Math.abs(input);
+// document.write("absolute value of "+input+" is "+absoluteValue);
+
+//Question-4
+// var random = Math.random();
+// var dice = Math.ceil(random*6);
+// document.write("random dice value: "+dice);
+
+//Question-5
+// var random = Math.random();
+// var dice = Math.ceil(random*2);
+// if (dice === 2) {
+//     document.write("2")
+//     document.write("<br>")
+//     document.write("random coin value: "+"Heads")
+// }else{
+//     document.write("1")
+//     document.write("<br>")
+//     document.write("random coin value: "+"Tails")
+// }
+
+//Question-6
+// var random = Math.random();
+// var roundValue = Math.round(random*100);
+// document.write("random value between 1 to 100: "+roundValue);
+
+//Question-7
+// var weightInput = prompt("Enter your weight in KG")
+// var floatingValue = parseFloat(weightInput);
+// document.write("The weight of the user is "+floatingValue+" Kilograms")
+
+//Question-8
+var num =5;
+var input = +prompt("Enter the secret number form 1 to 10")
+    if (input === num){
+        alert("Congratulation ")
+    }else{
+        alert("Try again!")
+    }
+
+    
+
+
